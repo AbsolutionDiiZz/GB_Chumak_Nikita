cb7ffc61640036a7232d133eb5f8ddd8d38da240
AbsolutionDiiZz <147521199+AbsolutionDiiZz@users.noreply.github.com>
2024-02-12 05:22:44 +0300

    Update AudioManager.cs
    add comments

--- Assets/Scripts/AudioManagers/AudioManager.cs
+++ Assets/Scripts/AudioManagers/AudioManager.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class AudioManager : MonoBehaviour
+public class AudioManager : MonoBehaviour // Управляет воспроизведением звуков и музыки в игре.
 {
     public static AudioManager Instance;
 
@@ -13,7 +13,7 @@ public class AudioManager : MonoBehaviour
     [SerializeField] private AudioClip asatruTheme;
     [SerializeField] private AudioClip hellenismTheme;
 
-    private void Awake()
+    private void Awake() // Воспроизведение музыки меню при запуске.
     {
         InitializeSingleton();
         PlayBackgroundMusic(mainTheme);
@@ -42,7 +42,7 @@ public class AudioManager : MonoBehaviour
         }
     }
 
-    private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    private void OnSceneLoaded(Scene scene, LoadSceneMode mode) 
     {
         switch (scene.buildIndex)
         {
@@ -55,39 +55,39 @@ public class AudioManager : MonoBehaviour
         }
     }
 
-    private void PlayGameSceneMusic()
+    private void PlayGameSceneMusic() // Выбор и воспроизведение музыки для игровой сцены в зависимости от конфессии.
     {
         AudioClip themeToPlay = MainMenuManager.currentConfession == "Hellenism" ? hellenismTheme : asatruTheme;
         PlayBackgroundMusic(themeToPlay);
     }
 
-    public void PlayClickSound()
+    public void PlayClickSound() // Воспроизведение звука клика.
     {
         effectsSource.PlayOneShot(clickSound);
     }
 
-    public void PlayBackgroundMusic(AudioClip musicClip)
+    public void PlayBackgroundMusic(AudioClip musicClip) // Воспроизведение музыкальной темы.
     {
         musicSource.clip = musicClip;
         musicSource.Play();
     }
 
-    public void AdjustMusicVolume(float volume)
+    public void AdjustMusicVolume(float volume) // Регулировка громкости музыки.
     {
         musicSource.volume = volume;
     }
 
-    public void AdjustEffectsVolume(float volume)
+    public void AdjustEffectsVolume(float volume) // Регулировка громкости звуковых эффектов.
     {
         effectsSource.volume = volume;
     }
 
-    public void ToggleMusic()
+    public void ToggleMusic() // Переключение включения/выключения музыки.
     {
         musicSource.mute = !musicSource.mute;
     }
 
-    public void ToggleEffects()
+    public void ToggleEffects() // Переключение включения/выключения звуковых эффектов.
     {
         effectsSource.mute = !effectsSource.mute;
     }
